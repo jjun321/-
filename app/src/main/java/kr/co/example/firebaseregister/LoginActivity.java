688f0d2a98585c44bfcc63541bdb598ae89b4d69
goalgoalin <sportsshouting@gmail.com>
2024-10-08 12:49:28 +0900

    first upload

---
+++ app/src/main/java/kr/co/example/firebaseregister/LoginActivity.java
@@ -0,0 +1,71 @@
+package kr.co.example.firebaseregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirevaseAuth; // 파이어 베이스 인증
+    private DatabaseReference mDatebaseRef;  // 실시간 데이터 베이스
+    private EditText mEtEmail, mEtPwd;
+    private Button mBtnRegister, mBtnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+        mFirevaseAuth = FirebaseAuth.getInstance();
+        mDatebaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pwd);
+        mBtnRegister = findViewById(R.id.btn_register);
+        mBtnLogin = findViewById(R.id.btn_login);
+
+        mBtnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+
+                mFirevaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    //로그인 성공
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    Toast.makeText(LoginActivity.this, "로그인 실패",Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                );
+            }
+        });
+        mBtnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/kr/co/example/firebaseregister/UserAccount.java
@@ -0,0 +1,36 @@
+package kr.co.example.firebaseregister;
+
+public class UserAccount {
+
+    private String idToken;
+    private String emailId;
+    private String password;
+
+    public UserAccount() {
+        // 기본 생성자
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

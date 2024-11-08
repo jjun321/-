04fbcffbc70ab3712e65ba0a14c8e2835eda0b8d
수종 <cssj2000@naver.com>
2024-11-08 15:27:30 +0900

    사이드 바

--- app/src/main/java/kr/co/example/firebaseregister/MainActivity.java
+++ app/src/main/java/kr/co/example/firebaseregister/MainActivity.java
@@ -1,18 +1,28 @@
-// MainActivity.java
 package kr.co.example.firebaseregister;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // DrawerLayout 초기화
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        // 왼쪽 드로어 메뉴 버튼
+        Button menuButton = findViewById(R.id.menu_button);
+        menuButton.setOnClickListener(v -> drawerLayout.openDrawer(Gravity.START));
 
         // 수능 버튼 클릭 이벤트 설정
         Button collegeEntranceExamButton = findViewById(R.id.college_entrance_exam);

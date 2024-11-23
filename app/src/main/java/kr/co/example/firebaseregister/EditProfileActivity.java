7ab41273a2d8bdcc14b8d17c36dc7b432668c33f
수종 <cssj2000@naver.com>
2024-11-23 17:14:36 +0900

    화면 이동
    아이콘 누르면 회원정보로 이동하고 회원정보 수정 누르면 edit_profile_xml로 이동하게 설정함

--- app/src/main/java/kr/co/example/firebaseregister/EditProfileActivity.java
+++ app/src/main/java/kr/co/example/firebaseregister/EditProfileActivity.java
@@ -1,16 +1,13 @@
-// EditProfileActivity.java
 package kr.co.example.firebaseregister;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditProfileActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile); // 레이아웃 파일을 나중에 생성할 것입니다
-
-        // 사용자 정보 수정 관련 로직을 여기에 추가합니다.
+        setContentView(R.layout.activity_edit_profile); // XML 파일 이름 확인
     }
 }

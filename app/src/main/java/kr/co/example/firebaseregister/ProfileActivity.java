ae5d1bffcda1a400858e782143779b82da1798b9
jjun321 <sportsshouting@gmail.com>
2024-11-30 16:02:07 +0900

    프로필화면,프로필넘기기화면수정

--- app/src/main/java/kr/co/example/firebaseregister/ProfileActivity.java
+++ app/src/main/java/kr/co/example/firebaseregister/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -92,7 +93,16 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-
+        // "arrowback" 버튼 클릭 시 MainActivity로 이동
+        ImageButton btnArrowback = findViewById(R.id.btnArrowback);
+        btnArrowback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ProfileActivity로 이동
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 

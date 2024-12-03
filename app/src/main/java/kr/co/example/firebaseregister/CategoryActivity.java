2699c52e5d234b16d16952a9f0d7b19af9098ee5
jjun321 <sportsshouting@gmail.com>
2024-12-03 16:24:16 +0900

    외국어,프로그래밍,필수자격증,편입,공무원 아이콘 등록

--- app/src/main/java/kr/co/example/firebaseregister/CategoryActivity.java
+++ app/src/main/java/kr/co/example/firebaseregister/CategoryActivity.java
@@ -44,17 +44,17 @@ public class CategoryActivity extends AppCompatActivity {
         }
         // 외국어 카테고리
         else if ("foreign_language".equals(categoryType)) {
-            elements.add(new CategoryElement("해커스", "https://www.hackers.com/", R.drawable.ic_foreign_language));
-            elements.add(new CategoryElement("YBM 어학원", "https://www.ybmedu.com/",R.drawable.ic_foreign_language));
-            elements.add(new CategoryElement("파고다", "https://www.pagoda21.com/",R.drawable.ic_foreign_language));
-            elements.add(new CategoryElement("월스트리트 잉글리쉬", "https://www.wallstreetenglish.co.kr/",R.drawable.ic_foreign_language));
+            elements.add(new CategoryElement("해커스", "https://www.hackers.com/", R.drawable.ic_hackers));
+            elements.add(new CategoryElement("YBM 어학원", "https://www.ybmedu.com/",R.drawable.ic_ybm));
+            elements.add(new CategoryElement("파고다", "https://www.pagoda21.com/",R.drawable.ic_pagoda));
+            elements.add(new CategoryElement("월스트리트 잉글리쉬", "https://www.wallstreetenglish.co.kr/",R.drawable.ic_wse));
         }
         // 프로그래밍 카테고리
         else if ("programming".equals(categoryType)) {
-            elements.add(new CategoryElement("인프런", "https://www.inflearn.com/", R.drawable.ic_programming));
-            elements.add(new CategoryElement("생활코딩", "https://opentutorials.org/", R.drawable.ic_programming));
-            elements.add(new CategoryElement("패스트캠퍼스", "https://www.fastcampus.co.kr/", R.drawable.ic_programming));
-            elements.add(new CategoryElement("코드스테이츠", "https://www.codestates.com/", R.drawable.ic_programming));
+            elements.add(new CategoryElement("인프런", "https://www.inflearn.com/", R.drawable.ic_inflearn));
+            elements.add(new CategoryElement("생활코딩", "https://opentutorials.org/", R.drawable.ic_lifecoding));
+            elements.add(new CategoryElement("패스트캠퍼스", "https://www.fastcampus.co.kr/", R.drawable.ic_fastcampus));
+            elements.add(new CategoryElement("코드스테이츠", "https://www.codestates.com/", R.drawable.ic_codestates));
         }
         // 필수 자격증 카테고리
         else if ("essential_license".equals(categoryType)) {
@@ -64,15 +64,15 @@ public class CategoryActivity extends AppCompatActivity {
         }
         // 편입 카테고리
         else if ("transfer".equals(categoryType)) {
-            elements.add(new CategoryElement("해커스 편입", "https://www.hackersut.com/?_C_=373827&utm_source=naver_ad_pc&utm_medium=brandsearch&utm_campaign=ut_ac_main&utm_content=hometext_a&utm_term=%ED%95%B4%EC%BB%A4%EC%8A%A4%ED%8E%B8%EC%9E%85&NaPm=ct%3Dm3cy48n4%7Cci%3D0zm0002SUN5ByRcNkKXk%7Ctr%3Dbrnd%7Chk%3D260da04781b2e5645437daebf3b07cef190532fa%7Cnacn%3Dr42dBMwHoZq4", R.drawable.ic_transfer));
-            elements.add(new CategoryElement("김영 편입", "https://www.kimyoung.co.kr/offmegaky.asp", R.drawable.ic_transfer));
-            elements.add(new CategoryElement("에듀윌 편입", "https://event.eduwill.net/Event/2024/_116/AllAcademyFreeTrial?etm_edwcd=_116&etm_mktcd=0000000100500100100100020002000275&utm_source=naver_ad_pc&utm_medium=univ_bs&utm_campaign=_1160000000100500100100100020002000275&utm_content=nad-a001-04-000000330140485&n_media=27758&n_query=%EC%97%90%EB%93%80%EC%9C%8C%ED%8E%B8%EC%9E%85&n_rank=1&n_ad_group=grp-a001-04-000000041613234&n_ad=nad-a001-04-000000330140485&n_keyword_id=nkw-a001-04-000006154504338&n_keyword=%EC%97%90%EB%93%80%EC%9C%8C%ED%8E%B8%EC%9E%85&n_campaign_type=4&n_contract=tct-a001-04-000000000962674&n_ad_group_type=5&NaPm=ct%3Dm3cy5bzs%7Cci%3D0zW0003FUN5B2JwZE13Y%7Ctr%3Dbrnd%7Chk%3D694f091578fc0fde36037bb4ffb61c40808fc8cf%7Cnacn%3Dr42dBMwHoZq4", R.drawable.ic_transfer));
+            elements.add(new CategoryElement("해커스 편입", "https://www.hackersut.com/?_C_=373827&utm_source=naver_ad_pc&utm_medium=brandsearch&utm_campaign=ut_ac_main&utm_content=hometext_a&utm_term=%ED%95%B4%EC%BB%A4%EC%8A%A4%ED%8E%B8%EC%9E%85&NaPm=ct%3Dm3cy48n4%7Cci%3D0zm0002SUN5ByRcNkKXk%7Ctr%3Dbrnd%7Chk%3D260da04781b2e5645437daebf3b07cef190532fa%7Cnacn%3Dr42dBMwHoZq4", R.drawable.ic_hackers));
+            elements.add(new CategoryElement("김영 편입", "https://www.kimyoung.co.kr/offmegaky.asp", R.drawable.ic_kimy));
+            elements.add(new CategoryElement("에듀윌 편입", "https://event.eduwill.net/Event/2024/_116/AllAcademyFreeTrial?etm_edwcd=_116&etm_mktcd=0000000100500100100100020002000275&utm_source=naver_ad_pc&utm_medium=univ_bs&utm_campaign=_1160000000100500100100100020002000275&utm_content=nad-a001-04-000000330140485&n_media=27758&n_query=%EC%97%90%EB%93%80%EC%9C%8C%ED%8E%B8%EC%9E%85&n_rank=1&n_ad_group=grp-a001-04-000000041613234&n_ad=nad-a001-04-000000330140485&n_keyword_id=nkw-a001-04-000006154504338&n_keyword=%EC%97%90%EB%93%80%EC%9C%8C%ED%8E%B8%EC%9E%85&n_campaign_type=4&n_contract=tct-a001-04-000000000962674&n_ad_group_type=5&NaPm=ct%3Dm3cy5bzs%7Cci%3D0zW0003FUN5B2JwZE13Y%7Ctr%3Dbrnd%7Chk%3D694f091578fc0fde36037bb4ffb61c40808fc8cf%7Cnacn%3Dr42dBMwHoZq4", R.drawable.ic_eduwill));
         }
         // 공무원 카테고리
         else if ("public_official".equals(categoryType)) {
             elements.add(new CategoryElement("공무원 저널", "https://www.psnews.co.kr/", R.drawable.ic_public));
             elements.add(new CategoryElement("공무원 합격의 길", "https://www.gonggantech.com/", R.drawable.ic_public));
-            elements.add(new CategoryElement("에듀윌 공무원", "https://www.eduwill.net/", R.drawable.ic_public));
+            elements.add(new CategoryElement("에듀윌 공무원", "https://www.eduwill.net/", R.drawable.ic_eduwill));
         }
 
         return elements;
